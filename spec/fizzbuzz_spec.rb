162cc25b07788db4aa828c8fb1e9964d8e29b769
Kareem <kareem335@yahoo.co.uk>
2019-06-10 14:53:32 +0100

    added one test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,14 +2,15 @@ require './lib/fizzbuzz'
 
 describe 'fizzbuzz' do
   it 'returns "fizz" for the number 3' do
-    expect(3.fizzbuzz).to eq 'Fizz'
+    expect(fizzbuzz(3)).to eq 'Fizz'
   end
   it 'returns "buzz" for the number 5' do
-    expect(5.fizzbuzz).to eq 'Buzz'
+    expect(fizzbuzz(5)).to eq 'Buzz'
   end
   it 'returns "fizzbuzz" when pass 3 & 5' do
-    expect(15.fizzbuzz).to eq 'FizzBuzz'
+    expect(fizzbuzz(15)).to eq 'FizzBuzz'
+  end
+  it 'check if reached 76 by returning 76' do
+    expect(fizzbuzz(76)).to eq 76
   end
 end
-
-expect 
